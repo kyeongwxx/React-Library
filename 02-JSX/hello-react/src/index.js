bd2cba034544f07ea3b67d36d6d270aa9a071b64
kyeongwxx <kyeongwxx@gmail.com>
2022-06-30 11:09:42 +0900

    2.3 JSX의 장점

--- 02-JSX/hello-react/src/index.js
+++ 02-JSX/hello-react/src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
+ReactDOM.render는 컴포넌트를 페이지에 렌더링하는 역할을 하며, react-dom 모듈을 불러와 사용할 수 있습니다. 이 함수의 첫 번째 파라미터에는 페이지에 렌더링할
+내용을 JSX 형태로 작성하고, 두 번째 파라미터에는 해당 JSX를 렌더링할 document 내부 요소를 설정합니다.
+*/
 root.render(
   <React.StrictMode>
     <App />
