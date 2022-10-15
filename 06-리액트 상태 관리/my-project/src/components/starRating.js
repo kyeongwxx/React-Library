b2da897a7eee11739a08cac9b5600ecb65d79eeb
kyeongwxx <kyeongwxx@gmail.com>
2022-10-15 22:03:32 +0900

    [06-리액트 상태 관리] 01-별점 컴포넌트 만들기

---
+++ 06-리액트 상태 관리/my-project/src/components/starRating.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaStar } from "react-icons/fa";
+
+const Star = ({ selected = false }) => (
+  <FaStar color={selected ? "red" : "grey"} />
+);
+
+const createArray = (length) => [...Array(length)];
+
+export default function StarRating({ totalStars = 5 }) {
+  return createArray(totalStars).map((n, i) => <Star key={i} />);
+}
