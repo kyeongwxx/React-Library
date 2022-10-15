296b4b9b3a479a87b7b09e4b82f1127ba7c02479
kyeongwxx <kyeongwxx@gmail.com>
2022-10-15 22:15:22 +0900

    [06-리액트 상태 관리] 02-useState 훅

--- 06-리액트 상태 관리/my-project/src/components/starRating.js
+++ 06-리액트 상태 관리/my-project/src/components/starRating.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-const Star = ({ selected = false }) => (
-  <FaStar color={selected ? "red" : "grey"} />
+const Star = ({ selected = false, onSelect = (f) => f }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
 const createArray = (length) => [...Array(length)];
 
 export default function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} / {totalStars}
+      </p>
+    </>
+  );
 }
