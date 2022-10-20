43889a83e14cf6e0c3c31d595c8a7cc0a0703bc9
kyeongwxx <kyeongwxx@gmail.com>
2022-10-20 16:40:45 +0900

    [07-훅스로 컴포넌트 개선하기] 06-컴포넌트 성능 개선

--- 07-훅스로 컴포넌트 개선하기/my-project/src/App.js
+++ 07-훅스로 컴포넌트 개선하기/my-project/src/App.js
@@ -1,40 +1,33 @@
-import React, { useReducer } from "react";
+import React, { memo, useReducer, useState } from "react";
 import "./App.css";
 
-const firstUser = {
-  id: "0391-3233-3201",
-  firstName: "Bill",
-  lastName: "Wilson",
-  city: "Missoula",
-  state: "Montana",
-  email: "bwilson@mtnwilsons.com",
-  admin: false
+const Cat = ({ name, meow = (f) => f }) => {
+  console.log(`rendering ${name}`);
+  return <p onClick={() => meow(name)}>{name}</p>;
 };
 
-const User = () => {
-  const [user, setUser] = useReducer(
-    (user, newDetails) => ({ ...user, ...newDetails}),
-    firstUser
-  )
-
-  return (
-    <div>
-      <h1>
-        {user.firstName} {user.lastName} - {user.admin ? "Admin" : "User"}
-      </h1>
-      <p>Email: {user.email}</p>
-      <p>
-        Location: {user.city}, {user.state}
-      </p>
-      <button onClick={() => {
-        setUser({ admin: true})
-      }}>Make Admin</button>
-    </div>
-  )
-}
+const PureCat = memo(
+  Cat,
+  (prevProps, nextProps) => prevProps.name === nextProps.name
+);
 
 function App() {
-  return <User />
+  const [cats, setCats] = useState(["Biscuit", "Jungle", "Outlaw"]);
+
+  return (
+    <>
+      {cats.map((name, i) => (
+        <PureCat
+          key={i}
+          name={name}
+          meow={(name) => console.log(`${name} has meowed`)}
+        />
+      ))}
+      <button onClick={() => setCats([...cats, prompt("Name a cat")])}>
+        Add a Cat
+      </button>
+    </>
+  );
 }
 
 export default App;
