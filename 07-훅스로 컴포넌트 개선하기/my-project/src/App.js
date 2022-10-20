3ff7c96045b239aad2aa6a7a832115eb865d7e9d
kyeongwxx <kyeongwxx@gmail.com>
2022-10-20 16:24:55 +0900

    [07-훅스로 컴포넌트 개선하기] 05-useReducer로 복잡한 상태 처리하기

--- 07-훅스로 컴포넌트 개선하기/my-project/src/App.js
+++ 07-훅스로 컴포넌트 개선하기/my-project/src/App.js
@@ -1,19 +1,40 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 
-const Checkbox = () => {
-  const [checked, toggle] = useReducer(checked => !checked, false)
-
-  return (
-    <>
-      <input type="checkbox" value={checked} onChange={toggle} />
-      {checked ? "checked" : "not checked"}
-    </>
-  );
+const firstUser = {
+  id: "0391-3233-3201",
+  firstName: "Bill",
+  lastName: "Wilson",
+  city: "Missoula",
+  state: "Montana",
+  email: "bwilson@mtnwilsons.com",
+  admin: false
 };
 
+const User = () => {
+  const [user, setUser] = useReducer(
+    (user, newDetails) => ({ ...user, ...newDetails}),
+    firstUser
+  )
+
+  return (
+    <div>
+      <h1>
+        {user.firstName} {user.lastName} - {user.admin ? "Admin" : "User"}
+      </h1>
+      <p>Email: {user.email}</p>
+      <p>
+        Location: {user.city}, {user.state}
+      </p>
+      <button onClick={() => {
+        setUser({ admin: true})
+      }}>Make Admin</button>
+    </div>
+  )
+}
+
 function App() {
-  return <Checkbox />;
+  return <User />
 }
 
 export default App;
